a49610af8c8c1c21ec2c18bba83866c41b17094b
arojas3552 <arojas@udel.edu>
2019-11-19 13:34:41 -0500

    Background is sized and working

--- level_1.py
+++ level_1.py
@@ -1,7 +1,7 @@
 import arcade
 
-SCREEN_HEIGHT = 1000
-SCREEN_WIDTH = 650
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 400
 SCREEN_TITLE = 'Endless Stairs'
 
 
@@ -14,3 +14,19 @@ class GameWindow(arcade.Window):
 
     def setup(self):
         self.background = arcade.load_texture("backgrounds/beach.png")
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+
+
+def main():
+    """ Main method """
+    window = GameWindow()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
