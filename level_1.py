7cba35163fbaf3457e6d9cebb1226a41e1a71807
arojas3552 <arojas@udel.edu>
2019-11-20 23:26:04 -0500

    Chciekn

--- level_1.py
+++ level_1.py
@@ -1,4 +1,5 @@
 import arcade
+from arcade.examples.sprite_move_animation import CHARACTER_SCALING
 
 SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 400
@@ -15,17 +16,20 @@ class GameWindow(arcade.Window):
     def setup(self):
         self.background = arcade.load_texture("backgrounds/beach.png")
 
-
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+
+
 class Sprite(arcade.Sprite):
-    def __init__ (self):
+    def __init__(self):
         self.player_list = None
+
     def setup(self):
         self.player_list = arcade.SpriteList()
         self.player_sprite = arcade.Sprite("", CHARACTER_SCALING)
+
 
 def main():
     """ Main method """
