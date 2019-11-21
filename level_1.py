c21a61e1bcdedcdef80dad846d9ad2a89f7170ce
courtney-brown7 <cbrown7973@gmail.com>
2019-11-21 00:15:22 -0500

    added sprite with proper scale

--- level_1.py
+++ level_1.py
@@ -3,7 +3,7 @@ import arcade
 SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 400
 SCREEN_TITLE = 'Endless Stairs'
-
+CHARACTER_SCALING = .05
 
 class GameWindow(arcade.Window):
 
@@ -14,18 +14,34 @@ class GameWindow(arcade.Window):
 
     def setup(self):
         self.background = arcade.load_texture("backgrounds/beach.png")
-
+        self.player_list = arcade.SpriteList()
+        self.player_sprite = arcade.Sprite("sprite/chicken.png", CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 120
+        self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-class Sprite(arcade.Sprite):
-    def __init__ (self):
+        self.player_list.draw()
+
+
+'''class Sprite(arcade.Sprite):
+    def __init__ (self): 
         self.player_list = None
     def setup(self):
-        self.player_list = arcade.SpriteList()
-        self.player_sprite = arcade.Sprite("", CHARACTER_SCALING)
+        
+       
+       
+
+    def on_draw(self):
+        arcade.start_render()
+
+        self.wall_list.draw()
+        self.coin_list.draw()
+
+'''
 
 def main():
     """ Main method """
