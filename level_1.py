addce406f9edc5dc7ddb403152bfa52b62910613
courtney-brown7 <cbrown7973@gmail.com>
2019-11-21 11:08:42 -0500

    added ground and first row of platforms

--- level_1.py
+++ level_1.py
@@ -1,9 +1,12 @@
 import arcade
 
-SCREEN_HEIGHT = 500
-SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 650
+SCREEN_WIDTH = 1000
 SCREEN_TITLE = 'Endless Stairs'
 CHARACTER_SCALING = .05
+TILE_SCALING = .05
+PLAYER_MOVEMENT_SPEED = 5
+
 
 class GameWindow(arcade.Window):
 
@@ -11,20 +14,69 @@ class GameWindow(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         self.background = None
+        self.wall_list = None
+        self.player_list = None
+        self.player_sprite = None
+        self.physics_engine = None
 
     def setup(self):
         self.background = arcade.load_texture("backgrounds/beach.png")
         self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+
         self.player_sprite = arcade.Sprite("sprite/chicken.png", CHARACTER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 120
         self.player_list.append(self.player_sprite)
 
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite("sprite/sand.jpg", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        coordinate_list = [[512, 96],
+                           [256, 96],
+                           [768, 96]]
+
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite("sprite/seashell.png", TILE_SCALING)
+            wall.position = coordinate
+            self.wall_list.append(wall)
+
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
-                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT,
+                                      self.background)
         self.player_list.draw()
+        self.wall_list.draw()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+        def on_key_release(self, key, modifiers):
+
+            if key == arcade.key.UP or key == arcade.key.W:
+                self.player_sprite.change_y = 0
+            elif key == arcade.key.DOWN or key == arcade.key.S:
+                self.player_sprite.change_y = 0
+            elif key == arcade.key.LEFT or key == arcade.key.A:
+                self.player_sprite.change_x = 0
+            elif key == arcade.key.RIGHT or key == arcade.key.D:
+                self.player_sprite.change_x = 0
+
+        def on_update(self, delta_time):
+
+            self.physics_engine.update()
 
 
 '''class Sprite(arcade.Sprite):
@@ -42,6 +94,7 @@ class GameWindow(arcade.Window):
         self.coin_list.draw()
 
 '''
+
 
 def main():
     """ Main method """
