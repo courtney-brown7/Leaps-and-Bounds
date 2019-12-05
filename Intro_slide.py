20650c072e3612b3abe8a29727ecdb76bd81f635
arojas3552 <arojas@udel.edu>
2019-12-04 23:33:54 -0500

    Added level 3, fixed sprites, connected level 1 and two errors.

---
+++ Intro_slide.py
@@ -0,0 +1,31 @@
+import arcade
+import level_1
+
+SCREEN_HEIGHT = 650
+SCREEN_WIDTH = 1000
+
+class IntroView(arcade.View):
+    def on_show(self):
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("LEVEL UP!!", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+                         arcade.color.BLACK, font_size=50, anchor_x="center")
+        arcade.draw_text("Click to advance", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 75,
+                         arcade.color.GRAY, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        first_level = level_1.Level1View()
+        self.window.show_view(first_level)
+
+def main():
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Different Views Example")
+    window.total_score = 0
+    intro_view = IntroView()
+    window.show_view(intro_view)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
